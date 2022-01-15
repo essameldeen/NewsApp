31c5edef68760accee9df7b7c3af2cc12456d8a6
essameldeen <essameldeen.ibrahim.stdsw@cis.asu.edu.eg>
2022-01-15 13:19:03 +0200

    1# setup project

---
+++ app/src/main/java/com/smartzone/newsapp/app/NewsApplication.kt
@@ -0,0 +1,6 @@
+package com.smartzone.newsapp.app
+
+import android.app.Application
+
+class NewsApplication : Application() {
+}
---
+++ app/src/main/java/com/smartzone/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.smartzone.utils
+
+abstract class Constant {
+    companion object {
+        const val API_KEY = "9cb870feabc147f5810cd5168161a290"
+        const val BASE_URL = "https://newsapi.org"
+        const val QUERY_PAGE_SIZE = 20
+
+    }
+}
